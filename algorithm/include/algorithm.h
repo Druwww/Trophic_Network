0d598f5be5357de2edfc27c84c56e23a0fe37e08
Quentin MULLIEZ <33511073+Druwww@users.noreply.github.com>
2018-04-06 13:32:06 +0200

    add commentary !

--- algorithm/include/algorithm.h
+++ algorithm/include/algorithm.h
@@ -10,88 +10,130 @@
 struct Algorithm{
 
     private:
+
+        //Pointeur of the graph's data
         Graph* m_graph;
 
+        //Vector who will have the index of our solution for kmin
         std::vector<int> m_vecIndexCombinaison;
 
+        //Vector who will have all Edge's pointeur of our graph
         std::vector<Edge*> m_vecLiaisonGraph;
 
+        //Vecteur who will have the edge's pointeurs of our solution for kEdgeMin
         std::vector<Edge*> m_vecKEdgeMin;
     public:
+        //Defauklt ctor
         Algorithm();
+        //Ctor with the graph's pointeurs
         Algorithm(Graph* graph);
+        //default dtor
         virtual ~Algorithm();
 
         //getter setter init
         Graph* getGraph() const {return m_graph;}
         void setGraph(Graph* val){m_graph = val;}
 
-        ///Algorithm fonctions
+        /***
+        Methodes use in all algorithm
+        ***/
+
+        //Clean all prossed for all nodes and edges
         void clearAllMarck();
 
+        //Return true if all nodes are prossed
         bool checkAllNodeMarck();
 
+        //Methode to clear a vecteur of type T
         template <typename T>
         void clearVecteur(std::vector<T>& vec);
 
         //Marquage composante fortement connexe
         void processedThreeOfNodeByNode(Node* nodeWork);
 
+        //Function of recurisive in BFS for see the strong connexity of two nodes
+        //d : data for the node in strudding
+        //down : true if we want to go down in the graph
         void processedThreeRecursive(data d, bool down);
 
+        //Return true if all the graph is strong connexe
         bool testStrongConnexeGraph();
 
+        //BFS to prossed the nodes and edges with strong connexe
         void processedGraphRecursive(data d);
 
-        //Trouver kmin
+        /***
+        Methode to find kmin : number of nodes to "kill" to not have a strong graph
+        ***/
+
+        //Methode who will active/desactivate all edge in function of the sommet kill or not
         int updateEdgesActive();
 
+        //Methode who will activate/desactive the edge passe in parameter
         bool updateEdgeActive(Edge* l);
 
+        //We pass a index of nodes to "kill", and it kill them
         void killNodeAttrsByIndex(std::vector<int> vecIndex);
 
+        //We pass a index of nodes to "revive", and it revive them
         void ReviveNodeAttrsByIndex(std::vector<int> vecIndex);
 
+        ///Algo/code retake from : https://stackoverflow.com/questions/12991758/creating-all-possible-k-combinations-of-n-items-in-c
+        //whill create all combinaison possible of k number take in vecIndex and after test them (call function)
+        //return true if the combination works and need to stop all recursive
         bool goKmin(int offset, int k, std::vector<int> vecIndex, std::vector<int>& combination);
 
+        ///Algo/code retake from : https://stackoverflow.com/questions/12991758/creating-all-possible-k-combinations-of-n-items-in-c
+        //whill check if the combination success to disconnect our graph
+        //return true if he success
         bool testCombinaisonKmin(std::vector<int> vectest);
 
+        //Function who whill find the kmin : number and which nodes need to be kill to disconect our graph
+        //display them in the consol and poressed them
         void findKmin();
 
+        //whill prossed all nodes with the indes in the vector of kmin nodes
         void processedKmin();
 
-        //trouver k connexe min
+        /***
+        Methode to find kEdgemin : number of edge to desactivate to not have a strong graph
+        ***/
+
+        //will put all graph edges' pointeur in the vector to analyse them one by one after
         void makeVecEdgeGraph();
 
+        //return true if the pointer pass in parameter is not in the vector
         bool testUnicEdgeInVec(Edge* edgeTest);
 
+        ///Algo/code retake from : https://stackoverflow.com/questions/12991758/creating-all-possible-k-combinations-of-n-items-in-c
+        //whill create all combinaison possible of k number take in vecIndex and after test them (call function)
+        //return true if the combination works and need to stop all recursive
         bool goKEdgeMin(int offset, int k, std::vector<int> vecIndex, std::vector<int>& combination);
 
+        ///Algo/code retake from : https://stackoverflow.com/questions/12991758/creating-all-possible-k-combinations-of-n-items-in-c
+        //whill check if the combination success to disconnect our graph
+        //return true if he success
         bool testCombinaisonKedgeMin(std::vector<int> vectest);
 
+        //We give him the index of edge to put their pointer in the vec of our kEdge min solution
         void saveEdgeByIndex(std::vector<int>& vecIndex);
 
+        //will desactivate all edges give in the vector of index
         void killEdgeByIndex(std::vector<int> vecIndex);
 
+        //will activate all edges give in the vector of index
         void reviveEdgeByIndex(std::vector<int> vecIndex);
 
+        //Function who whill find the kmin : number and which edges need to be desactivate to disconect our graph
+        //display them in the consol and poressed them
         void findKEdgeMin();
 
+        //whill prossed all edges with the indes in the vector of kmin nodes
         void processedKEdgemin();
 
-        ///Fonction temporaire
+        ///Functopn not permanente for display resulte in consol
         void displayNodeProssed();
 
 };
-
-
-
-
-
-
-
-
-
-
 
 #endif
